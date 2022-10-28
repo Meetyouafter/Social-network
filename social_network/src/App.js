01eed6f15f8b0a54c50e521dd70c68057c888e54
Levis <anton_levus@mail.ru>
2022-10-28 13:19:50 +0300

    add Body.jsx add Footer.jsx add Header.jsx add SideBar.jsx add FinallyForm.jsx edit App.css - add new classes for App edir Body.jsx - add start elements for app

--- social_network/src/App.js
+++ social_network/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import FinallyForm from './Components/FinallyForm';
 
 function App() {
   return (
     <div className="App">
-
+      <FinallyForm />
     </div>
   );
 }
---
+++ social_network/src/Components/FinallyForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Body from "./Body";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import '../App.css'
+
+
+const FinallyForm = () => {
+    return (
+        <div>
+            <Body />
+        </div>
+
+    )
+
+};
+
+export default FinallyForm;
